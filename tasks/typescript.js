2afdce306a91d447d89f154fc2bcd751170d5ae9
kmaru <k-maru@archway.co.jp>
2012-10-06 20:05:42 +0900

    複数ファイルの出力に対応

--- tasks/typescript.js
+++ tasks/typescript.js
@@ -8,19 +8,51 @@ module.exports = function (grunt) {
     var path = require('path'),
         fs = require('fs'),
         vm = require('vm'),
-        ts;
+        ts,
+        gruntIO = function (gruntPath, destPath, basePath) {
+            return {
+                createFile:function (writeFile) {
+                    var source = "";
+
+                    return {
+                        Write:function (str) {
+                            source += str;
+                        },
+                        WriteLine:function (str) {
+                            source += str + grunt.utils.linefeed;
+                        },
+                        Close:function () {
+
+                            if (source.trim().length < 1) {
+                                return;
+                            }
+                            if(basePath){
+                                var g = path.join(gruntPath, basePath);
+                                writeFile = writeFile.substr(g.length);
+                                writeFile = path.join(gruntPath, destPath, writeFile);
+                            }
+
+                            grunt.file.write(writeFile, source);
+                        }
+                    }
+                }
+            }
+        };
 
     grunt.registerMultiTask('typescript', 'Compile TypeScript files', function () {
         var dest = this.file.dest,
             options = this.data.options,
-            extension = this.data.extension;
+            extension = this.data.extension,
+            files = [];
 
         grunt.file.expandFiles(this.file.src).forEach(function (filepath) {
-            if(filepath.substr(-5) === ".d.ts"){
+            if (filepath.substr(-5) === ".d.ts") {
                 return;
             }
-            grunt.helper('compile', filepath, dest, grunt.utils._.clone(options), extension);
+            files.push(filepath);
         });
+
+        grunt.helper('compile', files, dest, grunt.utils._.clone(options), extension);
 
         if (grunt.task.current.errorCount) {
             return false;
@@ -29,9 +61,12 @@ module.exports = function (grunt) {
         }
     });
 
-    grunt.registerHelper('compile', function (src, destPath, options, extension) {
+    grunt.registerHelper('compile', function (srces, destPath, options, extension) {
 
-        var typeScriptPath = path.resolve(__dirname , '../node_modules/typescript/bin/typescript.js');
+        var typeScriptPath = path.resolve(__dirname, '../node_modules/typescript/bin/typescript.js'),
+            gruntPath = path.resolve("."),
+            basePath = options.base_path;
+
         if (!ts) {
             var code = fs.readFileSync(typeScriptPath);
             vm.runInThisContext(code, typeScriptPath);
@@ -39,12 +74,9 @@ module.exports = function (grunt) {
         }
 
         var outfile = {
-            source:'',
             Write:function (s) {
-                this.source += s;
             },
             WriteLine:function (s) {
-                this.source += s + "\r\n";
             },
             Close:function () {
             }
@@ -58,35 +90,27 @@ module.exports = function (grunt) {
             }
         };
 
-        if(options && options.module){
+        if (options && options.module) {
             var module = options.module.toLowerCase();
-            if(module === 'commonjs' || module === 'node') {
+            if (module === 'commonjs' || module === 'node') {
                 ts.moduleGenTarget = ts.ModuleGenTarget.Synchronous;
-            } else if(module === 'amd') {
+            } else if (module === 'amd') {
                 ts.moduleGenTarget = ts.ModuleGenTarget.Asynchronous;
             }
         }
 
         var setting = new ts.CompilationSettings();
-        var compiler = new ts.TypeScriptCompiler(outfile, outerr, undefined, setting);
+        var io = gruntIO(gruntPath, destPath, basePath);
+        var compiler = new ts.TypeScriptCompiler(io.createFile, outerr, undefined, setting);
         compiler.addUnit("" + fs.readFileSync(path.resolve(__dirname, '../node_modules/typescript/bin/lib.d.ts')),
             path.resolve(__dirname, '../node_modules/typescript/bin/lib.d.ts'), false);
-        compiler.addUnit("" + grunt.file.read(src), path.resolve(__dirname, '../../../', src), false);
-
-		compiler.typeCheck();
-        compiler.emit(false, function () {
-            return outfile;
+        srces.forEach(function (src) {
+            compiler.addUnit("" + grunt.file.read(src), path.resolve(gruntPath, src), false);
         });
 
-        var ext = path.extname(src);
-        var newFile;
-        if (ext) {
-            newFile = src.substr(0, src.length - ext.length) + ".js";
-        } else {
-            newFile = src + ".js";
-        }
-        grunt.file.write(path.join(destPath, newFile), outfile.source);
-        return true;
+        compiler.typeCheck();
+        compiler.emit(true, io.createFile);
 
+        return true;
     });
 };
